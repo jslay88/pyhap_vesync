6d8ccf8d77c4999b9134f452ebb62e682382de00
jslay <justin.slay@gmail.com>
2018-02-19 18:41:46 -0700

    Fixed typos

--- pyhap_vesync/app.py
+++ pyhap_vesync/app.py
@@ -6,10 +6,10 @@ from pyhap_vesync.utils.wrapper import Bridge
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Vesync HomeKit Bridge.'
+    parser = argparse.ArgumentParser(description='Vesync HomeKit Bridge. '
                                                  'Allows you to setup a Etekcity Vesync account '
                                                  'as a HomeKit Bridge for iOS devices.')
-    parser.add_argument('-u', '--username', type=str, required=True, help='Vesync Username (usually an email address.')
+    parser.add_argument('-u', '--username', type=str, required=True, help='Vesync Username (usually an email address.)')
     parser.add_argument('-p', '--password', type=str, required=True, help='Vesync Password')
 
     args = parser.parse_args()
