52cc42e77ed2c4673881f3af1e4ed7ae35ef54d4
jslay <justin.slay@gmail.com>
2018-06-25 23:44:20 -0600

    Fixed issue with positional arguement

--- pyhap_vesync/utils/wrapper/Bridge.py
+++ pyhap_vesync/utils/wrapper/Bridge.py
@@ -4,6 +4,6 @@ from .API import API
 
 class Bridge(HAPBridge):
     def __init__(self, username, password, display_name, *args, **kwargs):
-        super().__init__(display_name, *args, **kwargs)
+        super().__init__(display_name=display_name, *args, **kwargs)
         self.api = API(username, password)
         [self.add_accessory(outlet) for outlet in self.api.outlets]
